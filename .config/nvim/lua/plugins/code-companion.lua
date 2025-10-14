80543c8e3b3b099c584a002ccdc776d45afd50d6
s-sufi <53381168+saeed-sufi@users.noreply.github.com>
2025-10-14 12:40:22 +0330

    Update code-companion.lua

--- .config/nvim/lua/plugins/code-companion.lua
+++ .config/nvim/lua/plugins/code-companion.lua
@@ -1,33 +1,5 @@
 return {
   "olimorris/codecompanion.nvim",
-  opts = {},
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "ravitemer/mcphub.nvim",
-  },
-  config = function()
-    require("codecompanion").setup({
-      -- Add your configuration here
-      -- For example:
-      -- language = "python",
-      -- model = "gpt-3.5-turbo",
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            show_result_in_chat = true, -- Show mcp tool results in chat
-            make_vars = true, -- Convert resources to #variables
-            make_slash_commands = true, -- Add prompts as /slash commands
-
-          },
-        },
-      },
-    })
-  end,
-}
-return {
-  "olimorris/codecompanion.nvim",
   opts = {
 
     strategies = {
@@ -54,13 +26,4 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  -- config = function()
-  -- require("codecompanion").setup({
-  -- Add your configuration here
-  -- For example:
-  -- language = "python",
-  -- model = "gpt-3.5-turbo",
-  -- extensions = {},
-  -- })
-  -- end,
 }
