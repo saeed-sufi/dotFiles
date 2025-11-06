70ae4822c42d4a963c9520cd31594f8808ed4d16
s-sufi <53381168+saeed-sufi@users.noreply.github.com>
2025-11-06 09:12:21 +0330

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jk", "<Esc>", { desc = "Normal mode" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Normal mode" })
 -- vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close current tab" })
 -- vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "Rename Symbol" })
 vim.keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "Lists references to the symbol" })
